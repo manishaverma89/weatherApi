ee1db6351208972690f64a5317b556c47e45ded6
manisha <manisha.verma23@gmail.com>
2022-03-02 18:55:04 +0530

    Parse JSON

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { json } = require("express");
 const express = require("express");
 const https = require("https");     //Native Node https Module Tp Perform a get request
 const app = express();
@@ -8,7 +9,19 @@ app.get("/" , function(req,res){
    const url = "https://api.openweathermap.org/data/2.5/weather?&appid=2fd7f8813620b5e1839ebe6705007416&units=metric&q=yamunanagar"; 
 
    https.get(url , function(response){
-       console.log(response);
+       console.log(response.statusCode);
+
+       response.on("data" , function(data){
+        //    console.log(data);              // we'll get some hexadecimal code
+           const weatherData = JSON.parse(data);  // this will turn the data into actual javascript object
+        //    console.log(weatherData);
+           const temp = weatherData.main.temp; // will get the temp which in inside main 
+           console.log(temp);
+           const description = weatherData.weather[0].description;
+           console.log(description);
+
+       });
+
    });
 
    res.send("Server is up and running.");
