da082f4b57f64dc3e5479dcbaa5e91cf2a6799fb
manisha <manisha.verma23@gmail.com>
2022-03-03 07:55:10 +0530

    Working With Weather Icons And weather codes(ID)

--- app.js
+++ app.js
@@ -8,20 +8,27 @@ app.get("/" , function(req,res){
 
    const url = "https://api.openweathermap.org/data/2.5/weather?&appid=2fd7f8813620b5e1839ebe6705007416&units=metric&q=yamunanagar"; 
 
-   https.get(url , function(response){
+       https.get(url , function(response){
        console.log(response.statusCode);
 
        response.on("data" , function(data){
         //    console.log(data);              // we'll get some hexadecimal code
            const weatherData = JSON.parse(data);  // this will turn the data into actual javascript object
         //    console.log(weatherData);
+
            const temp = weatherData.main.temp; // will get the temp which in inside main 
         //    console.log(temp);
         res.write("<h1>Temperature is: " + temp + " degrees celcius " + " </h1> ");  //we can't use multiple send(),but we can use multiple res.write in combination with res.send().
+
            const description = weatherData.weather[0].description;
         //    console.log(description);
-
         res.write("<p>Description is: " + description + "</p>");
+
+        //  Adding Icon Image
+        const icon = weatherData.weather[0].icon;
+        const imageURL =  "http://openweathermap.org/img/wn/"+ icon + "@2x.png" ; //break the url with "const icon" 
+        res.write("<img src = " + imageURL +">");
+        // console.log(icon);
          res.send();
        });    
 
