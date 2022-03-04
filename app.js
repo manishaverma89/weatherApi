64e66284d5cb25b0d7f6d7905af72e18c749626e
manisha <manisha.verma23@gmail.com>
2022-03-04 06:38:35 +0530

    Using Body Parser To Parse Post Requests To The Server

--- app.js
+++ app.js
@@ -1,41 +1,79 @@
 const { json } = require("express");
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const https = require("https");     //Native Node https Module Tp Perform a get request
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.get("/" , function(req,res){
+   res.sendFile(__dirname + "/index.html");                                                         
 
-   const url = "https://api.openweathermap.org/data/2.5/weather?&appid=2fd7f8813620b5e1839ebe6705007416&units=metric&q=yamunanagar"; 
+});
+
+app.post("/",function(req,res){
+
+   // console.log(req.body.cityName);
+   const query = req.body.cityName;
+
+   const appKey = "2fd7f8813620b5e1839ebe6705007416";
+   const unit = "metric";
+   const url = "https://api.openweathermap.org/data/2.5/weather?&appid="+ appKey +"&units="+ unit +"&q=" + query;
+
+   // const url = "https://api.openweathermap.org/data/2.5/weather?&appid=2fd7f8813620b5e1839ebe6705007416&units=metric&q=yamunanagar";   //before breaking the url
+
 
        https.get(url , function(response){
        console.log(response.statusCode);
 
        response.on("data" , function(data){
         //    console.log(data);              // we'll get some hexadecimal code
-           const weatherData = JSON.parse(data);  // this will turn the data into actual javascript object
+          
+        const weatherData = JSON.parse(data);  // this will turn the data into actual javascript object
         //    console.log(weatherData);
 
-           const temp = weatherData.main.temp; // will get the temp which in inside main 
-        //    console.log(temp);
-        res.write("<h1>Temperature is: " + temp + " degrees celcius " + " </h1> ");  //we can't use multiple send(),but we can use multiple res.write in combination with res.send().
+    //Fetching Live Data From Open Weather Map API
+           
+           const weatherTemperature = weatherData.main.temp; // will get the temp which in inside main 
+         //   console.log(temperature);
+       
+        res.write("<h1>Temperature in " +query+ " is: " + weatherTemperature + " degrees celcius " + " </h1> ");  //we can't use multiple send(),but we can use multiple res.write in combination with res.send().
 
-           const description = weatherData.weather[0].description;
-        //    console.log(description);
-        res.write("<p>Description is: " + description + "</p>");
+           const weatherDesc = weatherData.weather[0].description;
+        //    console.log(weatherDesc);
+       
+        res.write("<p>Description is: " + weatherDesc + "</p>");
 
         //  Adding Icon Image
-        const icon = weatherData.weather[0].icon;
-        const imageURL =  "http://openweathermap.org/img/wn/"+ icon + "@2x.png" ; //break the url with const "icon" 
+        const iconImage = weatherData.weather[0].icon;
+        const imageURL =  "http://openweathermap.org/img/wn/"+ iconImage + "@2x.png" ; //break the url with const "icon" 
         res.write("<img src = " + imageURL +">");
         // console.log(icon);
          res.send();
        });    
 
-   });                                                              
+   });   
 
-//    res.send("Server is up and running.");
+
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
